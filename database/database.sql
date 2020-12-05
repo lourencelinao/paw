75f3b4dbce5b2bc1f4e8d370097fbacbc91fea39
Robert Tiempo <jroberttiempo58@gmail.com>
2020-12-05 23:30:30 +0800

    Update database
    Added outtake_id/intake_id in transferee, surrendered, rescued, transerred, and adopted tables.
    Connected vet and person tables.

--- database/database.sql
+++ database/database.sql
@@ -9,13 +9,13 @@ create table dog(
 	sex enum('Male', 'Female') NOT NULL,
 	weight decimal(5,2) NOT NULL,
 	color varchar(64) NOT NULL,
-	marks varchar(128) DEFAULT 'None',
-	aggressive enum('Yes', 'No') default 'No',
-	trained enum('Yes', 'No') default 'No',
-	status enum('Recuperating', 'Died in Care', 'Adopted', 'Euthanized', 'Transferred', 'Healthy') default 'Recuperating',
-	table_status enum('Active', 'Deleted') DEFAULT 'Active',
+	marks varchar(128) DEAULT 'None',
+	aggressive enum('Yes', 'No') DEAULT 'No',
+	trained enum('Yes', 'No') DEAULT 'No',
+	status enum('Recuperating', 'Died in Care', 'Adopted', 'Euthanized', 'Transferred', 'Healthy') DEAULT 'Recuperating',
+	table_status enum('Active', 'Deleted') DEAULT 'Active',
 	description text NOT NULL,
-	created datetime DEFAULT now(),
+	created datetime DEAULT now(),
 	updated datetime,
 	deleted datetime, 
 	
@@ -26,12 +26,12 @@ create table dog_image(
 	dog_image_id int auto_increment,
 	dog_id int NOT NULL,
 	image blob NOT NULL, 
-	table_status enum('Active', 'Deleted') DEFAULT 'Active',
-	created datetime DEFAULT now(),
+	table_status enum('Active', 'Deleted') DEAULT 'Active',
+	created datetime DEAULT now(),
 	deleted datetime,
 
-	constraint pk_dog_image_id primary key(dog_image_id),
-	constraint fk_dog_id foreign key(dog_id) references dog(dog_id)
+	constraint pk_dog_image primary key(dog_image_id),
+	constraint fk_dog foreign key(dog_id) references dog(dog_id)
 );
 
 create table person (
@@ -44,27 +44,26 @@ create table person (
 	address1 varchar(256) NOT NULL,
 	address2 varchar(256),
 	city varchar(50) NOT NULL,
-	table_status enum('Active', 'Deleted') DEFAULT 'Active',
-	created datetime DEFAULT now(),
+	table_status enum('Active', 'Deleted') DEAULT 'Active',
+	created datetime DEAULT now(),
 	updated datetime,
 	deleted datetime, 
 	
-	constraint pk_person_id primary key(person_id)
+	constraint pk_person primary key(person_id)
 );
 
 create table user(
 	user_id int auto_increment,
 	person_id int NOT NULL,
-	user_type enum('Admin', 'Manager', 'Staff', 'Volunteer') DEFAULT 'Admin', 
-	email varchar(128) not null,
+	user_type enum('Admin', 'Manager', 'Staff', 'Volunteer') DEAULT 'Admin', 
 	password varchar(256) NOT NULL,
-	table_status enum('Active', 'Deleted') DEFAULT 'Active',
-	created datetime DEFAULT now(),
+	table_status enum('Active', 'Deleted') DEAULT 'Active',
+	created datetime DEAULT now(),
 	updated datetime,
 	deleted datetime,
 	
-	constraint pk_user_id primary key(user_id),
-	constraint fk_user_id foreign key(person_id) references person(person_id)
+	constraint pk_user primary key(user_id),
+	constraint fk_user foreign key(person_id) references person(person_id)
 );
 
 create table clinic (
@@ -76,26 +75,25 @@ create table clinic (
 	province varchar(50) NOT NULL,
 	contact_number varchar(11) NOT NULL,
 	email_address varchar(50) NOT NULL,
-	table_status enum('Active', 'Deleted') DEFAULT 'Active',
-	created datetime DEFAULT now(),	
+	table_status enum('Active', 'Deleted') DEAULT 'Active',
+	created datetime DEAULT now(),	
 	updated datetime,
 	deleted datetime, 
 	
-	constraint pk_clinic_id primary key(clinic_id)
+	constraint pk_clinic primary key(clinic_id)
 );
 
 create table veterinarian (
 	vet_id int auto_increment,
+	person_id int NOT NULL,
 	clinic_id int,
-	name varchar(128) NOT NULL,
-	contact_number varchar(11) NOT NULL,
-	email_address varchar(50) NOT NULL,
-	table_status enum('Active', 'Deleted') DEFAULT 'Active',
-	created datetime DEFAULT now(),
+	table_status enum('Active', 'Deleted') DEAULT 'Active',
+	created datetime DEAULT now(),
 	deleted datetime, 
 	
-	constraint pk_vet_id primary key(vet_id),
-	constraint fk_vet_id foreign key(clinic_id) references clinic(clinic_id)
+	constraint pk_vet primary key(vet_id),
+	constraint fk1_vet foreign key(clinic_id) references clinic(clinic_id),
+	constraint fk2_vet foreign key(person_id) references person(person_id)
 );
 
 
@@ -110,14 +108,14 @@ create table medical_record(
 	action text NOT NULL,
 	medications text NOT NULL,
 	comment text NOT NULL,
-	table_status enum('Active', 'Deleted') DEFAULT 'Active',
-	created datetime DEFAULT now(),
+	table_status enum('Active', 'Deleted') DEAULT 'Active',
+	created datetime DEAULT now(),
 	deleted datetime, 
 
-	constraint pk_medical_id primary key(medical_id),
-	constraint fk1_medical_id foreign key(dog_id) references dog(dog_id),
-	constraint fk2_medical_id foreign key(clinic_id) references clinic(clinic_id),
-	constraint fk3_medical_id foreign key(vet_id) references veterinarian(vet_id)	
+	constraint pk_medical primary key(medical_id),
+	constraint fk1_medical foreign key(dog_id) references dog(dog_id),
+	constraint fk2_medical foreign key(clinic_id) references clinic(clinic_id),
+	constraint fk3_medical foreign key(vet_id) references veterinarian(vet_id)	
 );
 
 create table vaccination(
@@ -126,8 +124,8 @@ create table vaccination(
 	clinic_id int,
 	vet_id int,
 	vaccine_name varchar(128),
-	table_status enum('Active', 'Deleted') DEFAULT 'Active',
-	created DATETIME DEFAULT now(),
+	table_status enum('Active', 'Deleted') DEAULT 'Active',
+	created DATETIME DEAULT now(),
 	deleted DATETIME,
 	
 	constraint pk_vaccination primary key(vaccination_id),
@@ -139,108 +137,115 @@ create table vaccination(
 create table intake(
 	intake_id int auto_increment,
 	dog_id int NOT NULL,
-	intakeable_id int,
 	intakeable_type enum('Transferee', 'Surrendered', 'Rescued'),
-	health_condition text not null,
-	table_status enum('Active', 'Deleted') DEFAULT 'Active',
-	created datetime DEFAULT now(),
+	health_condition text NOT NULL,
+	table_status enum('Active', 'Deleted') DEAULT 'Active',
+	created datetime DEAULT now(),
 	deleted datetime,
 
-	constraint pk_intake_id primary key(intake_id),
-	constraint fk1_intake_id foreign key(dog_id) references dog(dog_id)
+	constraint pk_intake primary key(intake_id),
+	constraint fk1_intake foreign key(dog_id) references dog(dog_id)
 );
 
 
 create table transferee_intake(
 	transferee_id int auto_increment,
-	shelter_name varchar(128) not null,
+	intake_id int NOT NULL,
+	shelter_name varchar(128) NOT NULL,
 	address1 varchar(256) NOT NULL,
 	address2 varchar(256),
 	city varchar(50) NOT NULL,
 	province varchar(50) NOT NULL,
 	contact_number varchar(11) NOT NULL,
 	email_address varchar(50) NOT NULL,
-	table_status enum('Active', 'Deleted') DEFAULT 'Active',
-	created DATETIME DEFAULT now(),
+	table_status enum('Active', 'Deleted') DEAULT 'Active',
+	created DATETIME DEAULT now(),
 	deleted DATETIME,
 
-	constraint pk_transferee primary key(transferee_id)
+	constraint pk_transferee_intake primary key(transferee_id),
+	constraint fk_transferee_intake foreign key(intake_id) reerences intake(intake_id)
 );
 
 create table surrendered_intake(
 	surrendered_id int auto_increment,
-	person_id int,
-	reason_of_surrender text not null,
-	table_status enum('Active', 'Deleted') DEFAULT 'Active',
-	created DATETIME DEFAULT now(),
+	person_id int NOT NULL,
+	intake_id int NOT NULL,
+	reason_of_surrender text NOT NULL,
+	table_status enum('Active', 'Deleted') DEAULT 'Active',
+	created DATETIME DEAULT now(),
 	deleted DATETIME,
 
-	constraint pk_surrendered primary key(surrendered_id),
-	constraint fk_surrendered foreign key(person_id) references person(person_id)
+	constraint pk_surrendered_intake primary key(surrendered_id),
+	constraint fk1_surrendered_intake foreign key(person_id) references person(person_id),
+	constraint fk2_surrendered_intake foreign key(intake_id) references intake(intake_id)
 );
 
 create table rescued_intake(
 	rescued_id int auto_increment,
+	intake_id int NOT NULL,
 	address1 varchar(256) NOT NULL,
 	address2 varchar(256),
 	city varchar(50) NOT NULL,
 	province varchar(50) NOT NULL,
-	table_status enum('Active', 'Deleted') DEFAULT 'Active',
-	created DATETIME DEFAULT now(),
+	table_status enum('Active', 'Deleted') DEAULT 'Active',
+	created DATETIME DEAULT now(),
 	deleted DATETIME,
 
-	constraint pk_rescued primary key(rescued_id)
+	constraint pk_rescued_intake primary key(rescued_id),
+	constraint fk_rescued_intake foreign key(intake_id) references intake(intake_id)
 );
 
 create table outtake(
 	outtake_id int auto_increment,
 	dog_id int,
-	outtakeable_id int,
 	outtakeable_type enum('Transferred', 'Adopted') NOT NULL,
-	table_status enum('Active', 'Deleted') DEFAULT 'Active',
-	created datetime DEFAULT now(),
+	table_status enum('Active', 'Deleted') DEAULT 'Active',
+	created datetime DEAULT now(),
 	deleted datetime,
 
-	constraint pk_outtake_id primary key(outtake_id),
-	constraint fk1_outtake_id foreign key(dog_id) references dog(dog_id)
+	constraint pk_outtake primary key(outtake_id),
+	constraint fk_outtake foreign key(dog_id) references dog(dog_id)
 );
 
 create table transferred_outtake(
 	transferred_id int auto_increment,
-	shelter_name varchar(128) not null,
+	outtake_id int NOT NULL,
+	shelter_name varchar(128) NOT NULL,
 	address1 varchar(256) NOT NULL,
 	address2 varchar(256),
 	city varchar(50) NOT NULL,
 	province varchar(50) NOT NULL,
 	contact_number varchar(11) NOT NULL,
 	email_address varchar(50) NOT NULL,
-	table_status enum('Active', 'Deleted') DEFAULT 'Active',
-	created DATETIME DEFAULT now(),
+	table_status enum('Active', 'Deleted') DEAULT 'Active',
+	created DATETIME DEAULT now(),
 	deleted DATETIME,
 
-	constraint pk_transferred primary key(transferred_id)
+	constraint pk_transferred_outtake primary key(transferred_id),
+	constraint fk_transferred_outtake foreign key(outtake_id) references outtake(outtake_id)
 );
 
 create table adopted_outtake(
 	adopted_id int auto_increment,
-	person_id int,
-	table_status enum('Active', 'Deleted') DEFAULT 'Active',
-	created DATETIME DEFAULT now(),
+	person_id int NOT NULL,
+	outtake_id int NOT NULL,
+	table_status enum('Active', 'Deleted') DEAULT 'Active',
+	created DATETIME DEAULT now(),
 	deleted DATETIME,
 
-	constraint pk_adopted_id primary key(adopted_id),
-	constraint fk_adopted_id foreign key(person_id) references person(person_id)
+	constraint pk_adopted primary key(adopted_id),
+	constraint fk1_adopted foreign key(person_id) references person(person_id),
+	constraint fk2_adopted foreign key(outtake_id) references outtake(outtake_id)
 );
 
 
 create table action(
 	action_id int auto_increment,
-	user_id int,
-	actionable_id int not null,
-	actionable_type enum('Created', 'Updated', 'Deleted') not null,
-	table_status enum('Active', 'Deleted') DEFAULT 'Active',
-	created DATETIME DEFAULT now(),
-	deleted DATETIME,
+	user_id int NOT NULL,
+	table_id int NOT NULL,
+	table_name varchar(50) NOT NULL,
+	action_type enum('Created', 'Updated', 'Deleted') NOT NULL,
+	created DATETIME DEAULT now(),
 
 	constraint pk_action primary key (action_id),
 	constraint fk_action foreign key(user_id) references user(user_id)
