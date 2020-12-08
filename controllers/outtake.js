52789569ca4961cf47c978064feaea4ab45702a6
Robert Tiempo <jroberttiempo58@gmail.com>
2020-12-08 16:52:50 +0800

    Update module 2

--- controllers/outtake.js
+++ controllers/outtake.js
@@ -1,26 +1,29 @@
-const connection = require('../../database/database');
+const con = require('../../database/database');
 
 const getOuttake = (req, res) => {
-    connection.query("SELECT * FROM outtake WHERE table_status = 'Active'", (err, result)=>{
-        if(result){
-            res.send(result);
-        }
-    })
-}
+    let sql = `SELECT * 
+                FROM outtake 
+                WHERE table_status = "Active"`;
+    con.query(sql, (err, result)=>{
+        if(err) throw err;
+        res.send(result);
+    });
+};
 
 const addOuttake = (req, res) => {
-    connection.query("INSERT INTO outtake (dog_id, outtakeable_type) VALUES ('"+req.body.dog_id+"', '"+req.body.outtakeable_type+"')", (err, result)=>{
-        if(result){
-            res.send(result);
-        }
-    })
-}
+    let sql = `INSERT INTO outtake (dog_id, outtakeable_type) 
+                VALUES (${req.body.dog_id}, '${req.body.outtakeable_type}');`
+    con.query(sql, (err, result)=>{
+        if(err) throw err;
+    });
+};
 
 const deleteOuttake = (req, res) => {
-    connection.query("UPDATE outtake SET table_status = 'Deleted', deleted = now() WHERE outtake_id = '"+req.body.outtake_id+"'",(err, result)=>{
-        if(result){
-            res.send(result);
-        }
+    let sql = `UPDATE outtake 
+                SET table_status = 'Deleted', deleted = now() 
+                WHERE outtake_id = ${req.body.outtake_id}`;
+    con.query(sql, (err, result)=>{
+        if(err) throw err;
     })
 }
 
--- controllers/transferred_outtake.js
+++ controllers/transferred_outtake.js
@@ -1,31 +1,41 @@
-const connection = require('../../database/database');
+const con = require('../../database/database');
 
 const getTransferred_outtake = (req, res) => {
-    connection.query("SELECT * FROM transferred_outtake WHERE table_status = 'Active'", (err, result)=>{
-        if(result){
-            res.send(result);
-        }
-    })
-}
+    let sql = `SELECT * 
+                FROM transferred_outtake 
+                WHERE table_status = 'Active'`;
+    con.query(sql, (err, result)=>{
+        if(err) throw err;
+        res.send(result);
+    });
+};
 
 const addTransferred_outtake = (req, res) => {
-    connection.query("INSERT INTO transferred_outtake (outtake_id, shelter_name, address1, address2, city, province, contact_number, email_address) VALUES ('"+req.body.outtake_id+"', '"+req.body.shelter_name+"', '"+req.body.address1+"', '"+req.body.address2+"', '"+req.body.city+"', '"+req.body.province+"', '"+req.body.contact_number+"', '"+req.body.email_address+"')", (err, result)=>{
-        if(result){
-            res.send(result);
-        }
-    })
-}
+    let sql = `INSERT INTO transferred_outtake (outtake_id, shelter_name, address1, address2, city, province, contact_number, email_address) 
+                VALUES (${req.body.outtake_id}, 
+                        '${req.body.shelter_name}', 
+                        '${req.body.address1}', 
+                        '${req.body.address2}', 
+                        '${req.body.city}', 
+                        '${req.body.province}', 
+                        '${req.body.contact_number}', 
+                        '${req.body.email_address}')`;
+    con.query(sql, (err, result)=>{
+        if(err) throw err;
+    });
+};
 
 const deleteTransferred_outtake = (req, res) => {
-    connection.query("UPDATE transferred_outtake SET table_status = 'Deleted', deleted = now() WHERE transferred_id = '"+req.body.outtake_id+"'",(err, result)=>{
-        if(result){
-            res.send(result);
-        }
-    })
-}
+    let sql = `UPDATE transferred_outtake 
+                SET table_status = 'Deleted', deleted = now() 
+                WHERE transferred_id = ${req.body.outtake_id}`;
+    con.query(sql, (err, result)=>{
+        if(err) throw err;
+    });
+};
 
 module.exports = {
     getTransferred_outtake,
     addTransferred_outtake,
     deleteTransferred_outtake
-}
+};
