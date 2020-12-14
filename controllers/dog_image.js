aadd17ec64f5b83c06863c6a7c5afa5b9c0b71dd
Robert Tiempo <jroberttiempo58@gmail.com>
2020-12-14 16:05:40 +0800

    Updated dog_image

--- controllers/dog_image.js
+++ controllers/dog_image.js
@@ -1,11 +1,23 @@
 const con = require('../../database/database');
 
 const addDog_image = (req, res) => {
-    let sql = `INSERT INTO dog_image (dog_id, image) 
-                VALUES (${req.body.dog_id}, ${req.body.image})`;
-    con.query(sql,(err, result)=>{
-        if(err) throw err;
-    });
+    if(req.files){
+        let file = req.files.image;
+        let img_name = file.name;
+        
+        if(file.mimetype == "image/jpeg" || file.mimetype == "image/png"){
+            file.mv('directory'+file.name, (err)=>{
+                if(err) throw err;
+
+                let sql = `INSERT INTO dog_image (dog_id, image) 
+                            VALUES (${req.body.dog_id}, ${img_name})`;
+
+                con.query(sql,(err, result)=>{
+                    if(err) throw err;
+                });
+            })
+        }        
+    }    
 };
 
 const deleteDog_image = (req, res) => {
